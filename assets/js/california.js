f5892b11ecf9ef857fe2c18430c806e71bcf76bb
David Woolley <david.woolley@timesourceltd.com>
2020-10-14 09:05:34 +0000

    jQuery click event to #la working

--- assets/js/california.js
+++ assets/js/california.js
@@ -1,3 +1,6 @@
-$('#san-diego').click(function(){
-    $("#san-diego").hide();
+$('#la').click(function(){
+    $("#la-image").attr("src","./assets/images/disneyland.jpg");
 });
+$('#la').click(function(){
+    $("#la-text").html("<p>Hello Disney</p>");
+});    
