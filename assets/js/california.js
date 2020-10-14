dd645c2c83b9dd8e10fb026381a5eefe965dde14
David Woolley <david.woolley@timesourceltd.com>
2020-10-14 16:06:43 +0000

    google maps rendered with california markers added

--- assets/js/california.js
+++ assets/js/california.js
@@ -1,6 +1,25 @@
 $('#la').click(function(){
-    $("#la-image").attr("src","./assets/images/disneyland.jpg");
-});
+    $("#row-1-image").attr("src","./assets/images/disneyland.jpg");
+})
 $('#la').click(function(){
-    $("#la-text").html("<p>Hello Disney</p>");
-});    
+    $("#row-1-text").html("<p>Hello Disney</p>");
+})
+$('#la').click(function(){
+    $("#row-2-text").html("<p>Universal Studios</p>");
+})
+$('#la').click(function(){
+    $("#row-2-image").attr("src","./assets/images/universal-studios.jpg");
+});
+
+$('#san-diego').click(function(){
+    $("#row-1-image").attr("src","./assets/images/disneyland.jpg");
+})
+$('#san-diego').click(function(){
+    $("#row-1-text").html("<p>Hello Disney</p>");
+})
+$('#san-diego').click(function(){
+    $("#row-2-text").html("<p>Universal Studios</p>");
+})
+$('#san-diego').click(function(){
+    $("#row-2-image").attr("src","./assets/images/universal-studios.jpg");
+});
