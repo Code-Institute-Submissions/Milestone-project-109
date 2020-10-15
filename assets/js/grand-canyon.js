3261fd6f42ab9873017eab37cfe9c6212163cae7
David Woolley <david.woolley@timesourceltd.com>
2020-10-15 21:30:04 +0000

    grand canyon images and texts added

---
+++ assets/js/grand-canyon.js
@@ -0,0 +1,38 @@
+$('#hoover').click(function(){
+    $("#row-1-image").attr("src","./assets/images/hoover-damn.png");
+})
+$('#hoover').click(function(){
+    $("#row-1-text").html("<h3>The Hoover Damn</h3><p>On April 20, 1931, construction began on the Hoover Dam with it being completed on March 1, 1936. One of the greatest engineering achievements in  human history, Hoover Dam continues to draw crowds 80 years after its creation. Hoover Dam is about 30 miles south of Las Vegas on U.S. 93 and many day trips can be taken from there. The dam is named after America's 31st president, Herbert Hoover, who played a large role in bringing the nearby states into agreement about water allocations, settling a 25-year controversy. Completed in October 2010, the Mike O' Callaghan - Pat Tillman Memorial Bridge offers spectacular views that were once only available by helicopter....</p>");
+})
+$('#hoover').click(function(){
+    $("#row-2-text").html("<p>...Visitors have access to drive across the bridge and see the dam from a high vantage point. Located about 1,500 feet south of the Hoover Dam, the walkway on the bridge is approximately 900 feet above the Colorado River. One neat fact is that visitors can go from Nevada to Arizona after crossing the 1,900-foot-long bridge. There are plenty of informative placards before and during the bridge walk. Another interesting stop on the walk is the observation deck set above the dam affording panoramic views of the Colorado River and Lake Mead. The location also provides a bird's-eye view of the Hoover Dam facility, including an unobstructed look at the dam's mechanical components.</p>");
+})
+$('#hoover').click(function(){
+    $("#row-2-image").attr("src","./assets/images/hoover-damn2.png");
+});
+
+$('#skywalk').click(function(){
+    $("#row-1-image").attr("src","./assets/images/skywalk.png");
+})
+$('#skywalk').click(function(){
+    $("#row-1-text").html("<h3>Skywalk at the West Rim</h3><p>The Skywalk is about 120 miles east of Las Vegas, NV, and 70 miles north of Kingman, AZ. It can also be accessed via the Grand Canyon West Airport. Towering 4,000 feet above the Grand Canyon is the $30-million Skywalk, a horseshoe-shaped glass walkway. Built in 2007, the Skywalk gives you the feeling you are walking on air. In fact, you’ll see some tourists anxiously grab the handrails with their eyes shut. Don’t miss the exhibits on Hualapai history inside the Skywalk building.... </p>");
+})
+$('#skywalk').click(function(){
+    $("#row-2-text").html("<p>...It can be an adventure to get there, still, it's a one-of-a-kind way to experience the USA's most famous natural wonder. By car, go to Kingman in northwestern Arizona, which can be accessed by Interstate 40 or Highway 93. At Kingman, follow Highway 93/E Andy Devine Ave/B-40/Historic 66 for three or four miles, then continue on W Beale St/US-93 for a little more than 29 miles. Turn right at W Pierce Ferry Road and travel seven miles to Dolan Springs, where the Grand Canyon West Welcome Center is located. From there, it's 14 miles to the Skywalk. Visitors can take a 'Park and Ride' shuttle/coach from the Welcome Center.</p>");
+})
+$('#skywalk').click(function(){
+    $("#row-2-image").attr("src","./assets/images/skywalk2.png");
+});
+
+$('#south-rim').click(function(){
+    $("#row-1-image").attr("src","./assets/images/south-rim-visitor.png");
+})
+$('#south-rim').click(function(){
+    $("#row-1-text").html("<h3>The South Rim</h3><p>The south rim is perhaps the most popular entry point to the Grand Canyon and there's good reason. There are a vast array of activities available here that should be taken advantage of. There are some terrific Ranger Programs sponsored by National Park Service that include topics such as geology, animals and birds, fossils and cultural history. Whether you are driving, flying, biking or walking to get to the Grand Canyon, you come for the views. There are a variety of lectures or hikes guided by experienced rangers that will enhance your visit to the Grand Canyon. There is also a Junior Ranger Program for kids 6-14 years old.</p>");
+})
+$('#south-rim').click(function(){
+    $("#row-2-text").html("<p>The Rim Trail is 12 miles long on level, mostly paved road and is one of the easiest trails at the South Rim. The trail follows the rim and offers beautiful, panoramic views of the Canyon. There is NO water along this trail so hikers must be well-prepared. The Bright Angel Trail is a beautiful but strenuous trail that requires some planning and preparation. The trail is 9.2 roundtrip to Indian Garden, a welcoming refuge and natural spring in the midst of harsh terrain or you can hike another 3 miles further to Plateau Point. The 3,000 foot return to the rim is brutal for most hikers that underestimate the intense heat and rough landscape. The trans-canyon pipeline provides some water at the Mile-and-a-half Resthouse, the Three Mile Resthouse, and Indian Garden. </p>");
+})
+$('#south-rim').click(function(){
+    $("#row-2-image").attr("src","./assets/images/south-rim.png");
+});
