5980b16d03cc87c74f7c7d5ea64b07073624e8df
David Woolley <david.woolley@timesourceltd.com>
2020-10-17 18:09:05 +0000

    css and jquery files tidied and commented

--- assets/js/california.js
+++ assets/js/california.js
@@ -1,39 +1,39 @@
-$('#la').click(function(){
+$('#la').click(function(){ //changes first image to disneyland on california.html//
     $("#row-1-image").attr("src","./assets/images/disneyland.png");
 })
-$('#la').click(function(){
+$('#la').click(function(){ //changes text to disneyland text on california.html//
     $("#row-1-text").html("<h3>Disneyland California</h3><p>Just outside Los Angeles, Disneyland is California's premier family vacation destination, attracting visitors since the 1950s. Disneyland Park, with rides and experiences in elaborately created theme sets, is what most people picture when they imagine Disneyland. The Disneyland California Adventure Park, created during one of the expansions, holds even more action and adventure, with seven lands based on movie themes. Beyond the rides and Disney characters wandering the streets, the resort also offers a full complement of features and services for a complete vacation. Hotels, restaurants, shopping, and all kinds of entertainment options are available at Disneyland Resort.</p>");
 })
-$('#la').click(function(){
+$('#la').click(function(){ //changes text to universal studios text on california.html//
     $("#row-2-text").html("<h3>Universal Studios California</h3><p>Universal Studios Theme Park is known for its mind-blowing rides based on blockbuster movies, but it is also a working movie studio and an attraction everyone can enjoy. The highlight for most people is the ever-changing selection of rides, which range from simulators to roller-coasters. Favorite movie and TV-themed rides and sets include The Wizarding World of Harry Potter, The Walking Dead, The Simpsons, and Transformers. To save time standing in long lines consider purchasing a Skip the Line: Front of Line Pass at Universal Studios Hollywood. This handy ticket gives you one-time fast entry to each of the park's rides, attractions, and shows, as well as behind-the-scenes access to select attractions.</p>");
 })
-$('#la').click(function(){
+$('#la').click(function(){ //changes second image to universal studios on california.html//
     $("#row-2-image").attr("src","./assets/images/universal-studios.png");
 });
 
-$('#san-diego').click(function(){
+$('#san-diego').click(function(){ //changes first image to san diego zoo on california.html//
     $("#row-1-image").attr("src","./assets/images/san-diego-zoo.jpg");
 })
-$('#san-diego').click(function(){
+$('#san-diego').click(function(){ //changes text to san diego zoo text on california.html//
     $("#row-1-text").html("<h3>San Diego Zoo</h3><p>A top tourist attraction in Balboa Park, the San Diego Zoo is one of the largest and most famous zoos in the United States. Visiting this family-friendly attraction is also one of the top things to do in San Diego with kids. Plan at least a whole day here to really get the most out of your visit. You can see all kinds of exotic animals here, but perhaps its most famous residents are the giant pandas, which have been successfully bred here. Other favorite animals include gorillas, grizzly bears, koalas, leopards, bonobos, polar bears, rhinos, and sloths, and all the animal enclosures are designed to recreate natural settings for each species.</p>");
 })
-$('#san-diego').click(function(){
+$('#san-diego').click(function(){ //changes text to gaslamp quarter text on california.html//
     $("#row-2-text").html("<h3>The Gaslamp Quarter</h3><p>The Gaslamp Quarter National Historic District is the hub of the city's nightlife. It's a great place to visit if you're looking for things to do in downtown San Diego, especially after dark. Restored late-19th- and early-20th-century Victorian buildings house buzzing entertainment venues, trendy shops, art galleries, and restaurants serving every kind of cuisine you can imagine, from Persian food to pizza. This is a great place to catch some live jazz, comedy, or theater performances. Thanks to its great location in the heart of the city, the Gaslamp Quarter is also a great place to stay.</p>");
 })
-$('#san-diego').click(function(){
+$('#san-diego').click(function(){ //changes second image to gaslamp quarter on california.html//
     $("#row-2-image").attr("src","./assets/images/gaslamp-quarter.png");
 });
 
-$('#san-francisco').click(function(){
+$('#san-francisco').click(function(){ //changes first image to golden gate bridge on california.html//
     $("#row-1-image").attr("src","./assets/images/golden-gate.jpg");
 })
-$('#san-francisco').click(function(){
+$('#san-francisco').click(function(){ //changes text to golden gate bridge text on california.html//
     $("#row-1-text").html("<h3>The Golden Gate Bridge</h3><p>The Golden Gate Bridge is a California icon gracing San Francisco Bay. If you want to drive over the Golden Gate Bridge, the road is US Hwy 101, or SR 1, and walkways on either side of the bridge are open to pedestrians and cyclists. The bridge is only open to pedestrians and cyclists during daylight hours. For a great view of the bridge, or for anyone interested in photographing the bridge, there are a number of ideal vantage points.</p>");
 })
-$('#san-francisco').click(function(){
+$('#san-francisco').click(function(){ //changes text to alcatraz text on california.html//
     $("#row-2-text").html("<h3>Alcatraz</h3><p>The historic and notorious Alcatraz penitentiary, located on Alcatraz Island in San Francisco Bay, is one of America's most infamous prisons. It operated for almost thirty years, closing in 1963 and re-opening as a tourist attraction in 1973. Some of America's most well-known criminals were inmates here, including Al Capone and the 'Birdman', who would later form the basis for the fictional movie The Birdman of Alcatraz.</p>");
 })
-$('#san-francisco').click(function(){
+$('#san-francisco').click(function(){ //changes second image to alcatraz on california.html//
     $("#row-2-image").attr("src","./assets/images/alcatraz.png");
 });
 
--- assets/js/florida.js
+++ assets/js/florida.js
@@ -1,38 +1,38 @@
-$('#miami').click(function(){
+$('#miami').click(function(){ //changes first image to miami beach on florida.html//
     $("#row-1-image").attr("src","./assets/images/miami-beach.png");
 })
-$('#miami').click(function(){
+$('#miami').click(function(){ //changes text to miami beach text on florida.html//
     $("#row-1-text").html("<h3>Miami Beach</h3><p>Located on a barrier island and connected to the mainland by a series of bridges, Miami Beach is a mix of quiet neighbourhoods, lively entertainment-focused areas, and long stretches of soft-sand beaches. For visitors, South Beach and the Art Deco Historic District is one of the main highlights. This area features numerous beachfront restaurants, shops, hotels, and plenty of sunbathing opportunities. The most popular street in this area is Ocean Drive, a section of road located along the oceanfront and home to some beautiful Art Deco buildings.</p>");
 })
-$('#miami').click(function(){
+$('#miami').click(function(){ //changes text to miami nightlife text on florida.html//
     $("#row-2-text").html("<h3>Miami Nightlife</h3><p>Miami nightlife proves that some of the best things to do in Miami happen after dark. This city comes alive when the lights are low, the music is up and the drinks are flowing. Miami is a city known for amazing nightlife, and no wonder: the best clubs in Miami are among the best in the world, hedonistic havens where you can sway to Latin beats or throw down to thumping tunes from the world’s biggest DJs. But there’s more to Miami nightlife than that: There are also the best bars in Miami, which consist of late-night eateries, splashy cocktail dens and LGBT hot spots that keep us partying well after the sun sets on South Beach. So don't be afraid to get a little nocturnal.</p>");
 })
-$('#miami').click(function(){
+$('#miami').click(function(){ //changes second image to miami at night on florida.html//
     $("#row-2-image").attr("src","./assets/images/miami-nightlife.png");
 });
 
-$('#disney-world').click(function(){
+$('#disney-world').click(function(){ //changes first image to disney pic on florida.html//
     $("#row-1-image").attr("src","./assets/images/disney-world.png");
 })
-$('#disney-world').click(function(){
+$('#disney-world').click(function(){ //changes text to disney text on florida.html//
     $("#row-1-text").html("<h3>Disneyworld Resort</h3><p>For the ultimate amusement park experience, there's no place quite like Disney World in Orlando. The magic of Disney has not worn off over the decades as millions of visitors still flock here each year to enjoy the Magic Kingdom and the many other attractions of this mega amusement park. The number of things to do and see here has grown over the years and now includes an extensive list that reaches far beyond the traditional theme parks. If you're looking for more ideas, consider checking out Epcot, the water parks of Blizzard Beach and Typhoon Lagoon, the exotic animals of Animal Kingdom Park, or Disney Hollywood Studios. You'll also find all kinds of dining experiences, shopping areas, and shows.</p>");
 })
-$('#disney-world').click(function(){
+$('#disney-world').click(function(){ //changes text to disney text part 2 on florida.html//
     $("#row-2-text").html("<h3>Epcot</h3><p>Epcot is essentially two parks in one. On one side, Future World boasts attractions housed within separate buildings themed to space travel, imagination, and aquatic life. On the other side, Epcot’s World Showcase features 11 “pavilions” themed to countries with respective dining, entertainment, and attractions, set on an idyllic waterfront lagoon. Children of all ages can find something to enjoy at Epcot (children under 3 enter for free), but with its plentiful bars, entertainment, and festivals, adults will find World Showcase especially appealing. Summertime and the winter holidays are peak seasons at Disney World parks, but Epcot Center also draws crowds for its seasonal festivals.</p>");
 })
-$('#disney-world').click(function(){
+$('#disney-world').click(function(){ //changes second image to epcot pic on florida.html//
     $("#row-2-image").attr("src","./assets/images/epcot.png");
 });
 
-$('#orlando').click(function(){
+$('#orlando').click(function(){ //changes first image to seaworld pic on florida.html//
     $("#row-1-image").attr("src","./assets/images/sea-world.png");
 })
-$('#orlando').click(function(){
+$('#orlando').click(function(){ //changes text to seaworld text on florida.html//
     $("#row-1-text").html("<h3>Sea World</h3><p>Much like Walt Disney World, SeaWorld Orlando is a mega amusement park that has long been an important part of family vacations to Florida. The complex features its classic dolphin and whale shows, as well as acts featuring a cast of unexpected characters, from cats and ducks to otters and sea lions. Other highlights include the Antarctica: Empire of the Penguin display and a host of other aquarium exhibits offering opportunities to see all kinds of marine life. Thrill rides, like the high-speed Mako roller coaster, which reaches speeds of up to 73 miles per hour, and the Journey to Atlantis water coaster ride, are also a big part of the SeaWorld experience.</p>");
 })
-$('#orlando').click(function(){
+$('#orlando').click(function(){ //changes text to univversal islands of adventure text on florida.html//
     $("#row-2-text").html("<h3>Universal Islands of Adventure</h3><p>Universal’s Islands of Adventure is one of the most popular and highest-rated theme parks in Florida, and has received international awards for its range of experiences and attractions. The list of family-friendly attractions at this huge park is long enough to last a few days (thankfully multi-day passes are also available where you save more for each extra day you buy). The park is separated into 8 different zones – or ‘islands.’ These include the hugely popular The Wizarding World of Harry Potter-Hogsmeade, Marvel Super Hero Island, The Lost Continent, Port of Entry, Toon Lagoon, Jurassic Park, Skull Island, and Seuss Landing</p>");
 })
-$('#orlando').click(function(){
+$('#orlando').click(function(){ //changes sccond image to universal island pic on florida.html//
     $("#row-2-image").attr("src","./assets/images/universal-island.png");
 });
